47f99f9d5487bcaa8818a1e948e47a57cc568d53
Ismael <IJime31@gmail.com>
2024-03-14 13:28:26 -0600

    Cambio de lugar a previo 1
    Signed-off-by: Ismael <IJime31@gmail.com>

--- holaMundo.cpp
+++
@@ -1,6 +0,0 @@
-// Primer programa en C++
-#include <iostream>
-int main() {
-    std::cout << "Hola mundo, estudiantes de IE0217"; 
-    return 0; 
-}
--- otroArchivo.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-int main() {
-    std::cout << "Hola mundo, desde otro archivo!\n"; 
-    std::cout << "Esta es otra linea\n"; 
-    return 0; 
-}
