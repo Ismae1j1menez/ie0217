4ae3f982ab2df01979d47854653edd2003a585b0
Ismael <IJime31@gmail.com>
2024-03-14 13:14:27 -0600

    Organización del repositorio para entregas
    Signed-off-by: Ismael <IJime31@gmail.com>

---
+++ Previos/Previo1/holaMundo.cpp
@@ -0,0 +1,9 @@
+// Primer programa en C++
+// Este primer código se utiliza para practicar comandos de GitHub
+#include <iostream>
+// Funcion principal
+int main() {
+    std::cout << "Hola mundo, estudiantes de IE0217\n"; 
+    return 0; 
+    // No retorna nada
+}
---
+++ Previos/Previo1/otroArchivo.cpp
@@ -0,0 +1,8 @@
+// Este progrma es utilizado para poder comparar algunos comando de GitHub
+#include <iostream>
+// Función principal con la impresion de dos mensajes en consola
+int main() {
+    std::cout << "Hola mundo, desde otro archivo!\n"; 
+    std::cout << "Esta es otra linea\n"; 
+    return 0; 
+}
