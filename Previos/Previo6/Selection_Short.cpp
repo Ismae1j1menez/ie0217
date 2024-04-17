c478b7659d840b954bdb346ca119be41d2db02f3
Ismael <IJime31@gmail.com>
2024-04-16 22:32:56 -0600

    Se añade el algoritmo Selection short
    Signed-off-by: Ismael <IJime31@gmail.com>

---
+++ Previos/Previo6/Selection_Short.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+// Función swap
+void swap(int *a, int *b){
+    int temp = *a;  
+    *a = *b; 
+    *b = temp;  
+}
+
+// Función para imprimir el array
+void printArray(int array[], int size){
+    for (int i = 0; i < size; i++){
+        cout << array[i] << " ";  
+    }
+    cout << endl; 
+}
+
+// Función para ordenar el array con el algoritmo de Selection Sort
+void selectionSort(int array[], int size){
+    for(int step = 0; step < size - 1; step++){
+        int min_idx = step; 
+        for (int i = step + 1; i < size; i++){
+            // Selecciona el elemento menor de cada loop
+            if (array[i] < array[min_idx])
+                min_idx = i;
+        }
+        swap(&array[min_idx], &array[step]);
+    }
+}
+
+// Prueba el funcionamiento del algoritmo
+int main(){
+    int data[] = {20,12,10,15,2};
+    int size = sizeof(data) / sizeof(data[0]);  
+    selectionSort(data, size); 
+    cout << "Sorted array in Ascending Order: \n";
+    printArray(data, size);
+}
