2461ef4b764df68cd7aa8045ef679275a6b2b92a
Ismael <IJime31@gmail.com>
2024-06-20 09:22:53 -0600

    Todos los comandos del sql
    Signed-off-by: Ismael <IJime31@gmail.com>

--- Tareas/Tarea6/Tarea6.sql
+++ Tareas/Tarea6/Tarea6.sql
@@ -148,4 +148,157 @@ SELECT * FROM Requisitos;
 -- Antes de agregar nuevos cursos inventados
 SELECT * FROM Cursos;
 
+-- Agregar nuevos cursos inventados
+INSERT INTO Cursos (Sigla, Nombre, Semestre, Creditos) VALUES
+('IE-0628', 'Seguridad De La Información', 'I-2024', 3), 						
+('IE-0475', 'Bases De Datos', 'I-2024', 3);
 
+-- Después de agregar nuevos cursos inventados
+SELECT * FROM Cursos;
+
+-- Antes de agregar descripciones para los nuevos cursos
+SELECT * FROM Descripciones;
+
+-- Agregar descripciones para los nuevos cursos
+INSERT INTO Descripciones (CursoID, Descripcion, Dificultad) VALUES
+    ((SELECT CursoID FROM Cursos WHERE Nombre = 'Seguridad De La Información'), 
+    'Principios y prácticas para asegurar la información en sistemas computacionales.', 
+    'Media'),
+
+    ((SELECT CursoID FROM Cursos WHERE Nombre = 'Bases De Datos'), 
+    'Fundamentos de diseño y manejo de bases de datos relacionales y no relacionales.', 
+    'Media');
+
+-- Después de agregar descripciones para los nuevos cursos
+SELECT * FROM Descripciones;
+
+-- Nntes de agregar requisitos para los nuevos cursos
+SELECT * FROM Requisitos;
+
+-- Insertar los requisitos de los nuevos cursos del plan
+INSERT INTO Requisitos (CursoID, RequisitoCursoID) VALUES
+    ((SELECT CursoID FROM Cursos WHERE Nombre = 'Seguridad De La Información'), 
+    (SELECT CursoID FROM Cursos WHERE Nombre = 'Circuitos Digitales II')),
+
+    ((SELECT CursoID FROM Cursos WHERE Nombre = 'Bases De Datos'), 
+    (SELECT CursoID FROM Cursos WHERE Nombre = 'Estructuras Abstractas de Datos y Algoritmos'));
+
+-- Después de agregar requisitos para los nuevos cursos
+SELECT * FROM Requisitos;
+
+-- Consultar todos los cursos y sus descripciones
+SELECT c.Sigla, c.Nombre, c.Semestre, c.Creditos, d.Descripcion, d.Dificultad
+FROM Cursos c
+JOIN Descripciones d
+ON c.CursoID = d.CursoID;
+
+-- Consultar los requisitos de un curso específico
+SELECT c.CursoID, c.Sigla, c.Nombre, GROUP_CONCAT(c1.Sigla SEPARATOR ', ') AS Requisitos
+FROM Cursos c
+JOIN Requisitos r
+ON c.CursoID = r.CursoID
+JOIN Cursos c1
+ON r.RequisitoCursoID = c1.CursoID
+WHERE c.Nombre = 'Electrónica industrial';
+
+-- Consultar para listar los cursos que no son optativos
+SELECT *
+FROM Cursos
+WHERE Nombre NOT LIKE 'Optativa%';
+
+-- Antes de actualizar el nombre y créditos de 3 cursos optativos
+SELECT * FROM Cursos;
+
+-- Actualizar el nombre y créditos de 3 cursos optativos
+UPDATE Cursos
+SET 
+    Sigla = 'IE-0621', 
+    Nombre = 'Verificación funcional del diseño de circuitos integrados', 
+    Semestre = 'I', 
+    Creditos = 3 
+WHERE 
+    Nombre = 'Optativa I';
+
+UPDATE Cursos
+SET 
+    Sigla = 'IE-0411', 
+    Nombre = 'Microelectrónica: Sistemas en Silicio', 
+    Semestre = 'II', 
+    Creditos = 3
+WHERE 
+    Nombre = 'Optativa II';
+
+UPDATE Cursos
+SET 
+    Sigla = 'IE-0623', 
+    Nombre = 'Microprocesadores', 
+    Semestre = 'II', 
+    Creditos = 3
+WHERE 
+    Nombre = 'Optativa III';
+
+-- Después de actualizar el nombre y créditos de 3 cursos optativos
+SELECT * FROM Cursos;
+
+-- Antes de actualizar la descripción y dificultad de 3 cursos existentes
+SELECT * FROM Descripciones;
+
+-- Actualizar la descripción y dificultad de 3 cursos existentes
+UPDATE Descripciones
+SET 
+    Descripcion = 'Implementación de algoritmos avanzados y estructuras de datos eficientes en C++.', 
+    Dificultad = 'Alta'
+WHERE 
+    CursoID = (SELECT CursoID FROM Cursos WHERE Nombre = 'Estructuras Abstractas de Datos y Algoritmos');
+
+UPDATE Descripciones
+SET 
+    Descripcion = 'Diseño de sistemas digitales avanzados y verificación usando Verilog.', 
+    Dificultad = 'Alta'
+WHERE 
+    CursoID = (SELECT CursoID FROM Cursos WHERE Nombre = 'Circuitos Digitales II');
+
+UPDATE Descripciones
+SET 
+    Descripcion = 'Desarrollo de habilidades para la propuesta y preparación del Trabajo Final de Graduación.', 
+    Dificultad = 'Alta'
+WHERE 
+    CursoID = (SELECT CursoID FROM Cursos WHERE Nombre = 'Anteproyecto de TFG');
+
+-- Después de actualizar la descripción y dificultad de 3 cursos existentes
+SELECT * FROM Descripciones;
+
+-- Antes de eliminar un curso inventado y 2 cursos del plan y sus descripciones asociadas
+SELECT * FROM Cursos;
+SELECT * FROM Descripciones;
+
+-- Eliminar un curso inventado y 2 cursos del plan y sus descripciones asociadas
+DELETE FROM Cursos 
+WHERE Nombre = 'Seguridad De La Información';
+
+DELETE FROM Cursos 
+WHERE Nombre = 'Administración de sistemas';
+
+DELETE FROM Cursos 
+WHERE Nombre = 'Electrónica industrial';
+
+-- Después de eliminar un curso inventado y 2 cursos del plan y sus descripciones asociadas
+SELECT * FROM Cursos;
+SELECT * FROM Descripciones;
+
+-- Antes de eliminar requisitos de 2 cursos específicos
+SELECT * FROM Requisitos;
+
+-- Eliminar requisitos de 2 cursos específicos
+DELETE FROM Requisitos
+WHERE 
+    CursoID = (SELECT CursoID FROM Cursos WHERE Nombre = 'Ciencia de datos para la est. y pron. de eventos') 
+    AND RequisitoCursoID = (SELECT CursoID FROM Cursos WHERE Nombre = 'Modelos Probabilísticos de Señales y Sistemas');
+
+DELETE FROM Requisitos
+WHERE 
+    CursoID = (SELECT CursoID FROM Cursos WHERE Nombre = 'Seguridad Ocupacional') 
+    AND RequisitoCursoID = (SELECT CursoID FROM Cursos WHERE Nombre = 'Responsabilidades en el Ejercicio Profesional de la Ingeniería Eléctrica');
+
+-- Después de eliminar requisitos de 2 cursos específicos
+SELECT * FROM Requisitos;
